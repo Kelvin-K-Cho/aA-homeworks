e15ca3c0d3079c46a14f47f587fc5d9343812160
Kelvin Cho <cho.k.kelvin@gmail.com>
2017-12-08 22:46:05 -0800

    Added W2D1 files

---
+++ W2D1/simon/lib/simon.rb
@@ -0,0 +1,73 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+      system("clear")
+    end
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 1.00
+      system("clear")
+      sleep 0.5
+    end
+  end
+
+  def require_sequence
+    puts "Enter color"
+    @seq.each do |color|
+      user_color = gets.chomp
+      user_color.downcase!
+      if color != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.5
+  end
+
+  def add_random_color
+    @seq << COLORS.shuffle[0]
+  end
+
+  def round_success_message
+    puts "Beginning next round..."
+  end
+
+  def game_over_message
+    puts "Game over.  You completed #{@sequence_length - 1} rounds."
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
+
+simon = Simon.new
+simon.play
